2c2aeca1cce2523492ddfad75274b16504f02577
uvcanaditya <uvcan.aditya@gmail.com>
2023-08-31 23:11:45 +0530

    Local Mannual Auth for sing in a user

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -1,9 +1,18 @@
 const User=require('../models/user');
 
-module.exports.user=function(req,res){
-    return res.render('user_profile',{
-        title:'Techbook  |User-profile'
-    });
+module.exports.profile=function(req,res){
+   if(req.cookie.user_id){
+        if(user){
+            return res.render('user-profile',{
+                title:'User Profile',
+                user:user
+            });
+        } else{
+                res.redirect('user/sigh-in');
+            }
+    }else{
+        res.redirect('sigh-in');
+    }
 }
 
 // render the sighUp page
@@ -22,7 +31,7 @@ module.exports.sighIn=function(req,res){
 
 // get the singh UP data
 module.exports.create=function(req,res){
-    // TO DO LATER
+    // Sigh Up the user
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
@@ -45,4 +54,24 @@ module.exports.create=function(req,res){
 //sighIn and creating a session for the user
 module.exports.createSession=function(req,res){
     // TO DO LATER
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('erroe in finding the user');return};
+        //Handle the user
+        if(user){
+            //Handle password mathch
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //stablishing th eidentity
+            res.cookie('user-id',user.id);
+            return res.redirect('user/profile');
+        }
+        else{
+            // handle user not found 
+            return res.redirect('back');
+        }
+    });
+
+
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express=require('express');
 const router=express.Router();
 const usersController=require('../contollers/users_controller');
 
-router.get('/profile',usersController.user);
+router.get('/profile',usersController.profile);
 router.get('/sigh-up',usersController.sighUp);
 router.get('/sigh-in',usersController.sighIn);
 router.post('/create',usersController.create);
+router.get('create-session',usersController.createSession);
 
 module.exports=router;
