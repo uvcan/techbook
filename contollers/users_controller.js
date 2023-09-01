b8db9de515bda66e256c6c7f360a6a7b561e9831
uvcanaditya <uvcan.aditya@gmail.com>
2023-09-02 01:56:05 +0530

    Permanently storing data in MongoDb and sigh-In

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -30,26 +30,25 @@ module.exports.sighIn=function(req,res){
 }
 
 // get the singh UP data
-module.exports.create=function(req,res){
-    // Sigh Up the user
-    if(req.body.password != req.body.confirm_password){
+module.exports.create = async function(req, res) {
+    try {
+      if (req.body.password !== req.body.confirm_password) {
         return res.redirect('back');
+      }
+  
+      const existingUser = await User.findOne({ email: req.body.email });
+  
+      if (!existingUser) {
+        const newUser = await User.create(req.body);
+        return res.redirect('/users/sign-in');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (error) {
+      console.error('Error during user signup:', error);
+      return res.status(500).send('Internal Server Error'); // Handle the error appropriately
     }
-    User.findOne({email:req.body.email},function(err,user){
-        if(err){console.log('Error in finding the user');return};
-
-        if(!user){
-            User.create(req.body,function(err,user){
-                if(err){console.log('Error in finding the user');return};
-                return res.redirect('user/sigh-in');
-            });
-        }
-        else{
-            return res.redirect('back');
-        }
-    })
-    
-}
+  };
 
 //sighIn and creating a session for the user
 module.exports.createSession=function(req,res){
